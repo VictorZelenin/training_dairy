0f38f16f66a72eb48db35221bfe18a2118483f75
victor <zelenin.victor95@gmail.com>
2016-05-17 01:17:35 +0200

    have been started write a gui

--- src/main/java/data/dao/ExercisesInTrainingDAO.java
+++ src/main/java/data/dao/ExercisesInTrainingDAO.java
@@ -4,6 +4,7 @@ import data.data_sets.Exercise;
 import data.data_sets.Training;
 import database_service.executor.Executor;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
